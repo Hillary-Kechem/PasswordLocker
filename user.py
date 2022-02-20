d4dfb8bef2fbf18f6e343643f05b08ae28cef981
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-20 22:53:07 +0300

    user class generation

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    """
+    class that will generate new instances of users 
+    """
+
+    def __init__(self,login_username):
+        """ 
+        the __init__method helps define properties of our object
+        """
+
+        self.login_username = login_username
+
+        """
+        arguments for __init__method will this"""
